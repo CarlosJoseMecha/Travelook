ec874728e94372e9685c0bcbd5585cd602c94221
MarColoma <mar.coloma.generation@gmail.com>
2019-12-03 12:34:18 +0100

    mar comienzo api y json
    añadido comienzo js para la api, añadidos json con la ropa y pequeños cambios

--- js/appTiempo.js
+++ js/appTiempo.js
@@ -1,7 +1,7 @@
 
 var contenido = document.querySelector('#contenido');
 
-function traer() {
+/* function traer() {
   fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/71446b88e6eef2aa7de9e6488649d968/40.3160097,-3.8765372,1479294000?exclude=currently,flags,hourly&') //Traemos el archivo con fetch
     .then(data => data.text()) //Hacemos una promesa de que lo haga y con data text, lo convertimos a txt
     .then(data => {
@@ -17,19 +17,61 @@ function traer() {
   }
 
   traer();
+ */
 
- 
+let ciudadDestino = document.querySelector('#destinoViaje');
 
-  function convertirFarenACelsius(farenheit){
-    var celsiusConvertir = (farenheit - 32) * 5 / 9;
-    var celsiusRedondeados = Math.round(celsiusConvertir * 100) / 100;
-    return celsiusRedondeados;
-  }
+
+function traer() {
+
+
+  fetch(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${ciudadDestino},ES&APPID=dcad6bd0f350bf23372a42cce21f47da`)
+
+    /*   fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=Mostoles,ES&APPID=dcad6bd0f350bf23372a42cce21f47da') */ //Traemos el archivo con fetch
+    .then(data => data.text()) //Hacemos una promesa de que lo haga y con data text, lo convertimos a txt
+    .then(data => {
+
+
+      contenido.innerHTML = `${data}` // Así nos traemos de manera literal una variable HTML
+      var viaje = JSON.parse(`${data}`);
+      console.log(viaje);
+      //var daily = viaje.data[0]//me creo esta variable para reutilizarla de manera + eficiente al acceder a datos
+      let destino = (viaje.name);
+      let temMax = (viaje.main.temp);
+      let temGEN = (viaje.sys.country);
+      let SEM = (viaje.weather[0].main);
+
+
+      contenido.innerHTML = destino + " " + temMax + " " + temGEN + " " + SEM;
+      //var celsiusPintar = convertirFarenACelsius(viaje.temMax); //estoy llamando a la funcion y recibiendo el resultado
+      //contenido.innerHTML = celsiusPintar.toLocaleString();
+    })
+}
+traer();
+
+
+
+
+
+function convertirFarenACelsius(farenheit) {
+  var celsiusConvertir = (farenheit - 32) * 5 / 9;
+  var celsiusRedondeados = Math.round(celsiusConvertir * 100) / 100;
+  return celsiusRedondeados;
+}
+
+
+
+
+
+
+
+
+
 
   //Array ciudades cuado pincho que me busque esa ciudad.
-/* 
+/*
 traer los dos valores .latirtud.Array
 
-var latitud 
+var latitud
 var longitud
 var ciudad (latitud, longirud) */
