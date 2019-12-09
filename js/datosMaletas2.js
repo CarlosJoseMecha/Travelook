3ea909d2c237aa9755cd07037637f06a52b34187
MarColoma <mar.coloma.generation@gmail.com>
2019-12-09 11:12:02 +0100

    introducido JSON mujer frio

--- js/datosMaletas2.js
+++ js/datosMaletas2.js
@@ -51,6 +51,12 @@ var mujerCalor = [];
 var mujerFrio = [];
 
 
+///json/hombre/calor/img/1.jpg"
+let genero = "mujer";
+let temperatura2 = "frio";
+
+
+
 
 var xhttp = new XMLHttpRequest(); //Pedimos una nueva request 
 
@@ -58,29 +64,31 @@ xhttp.onreadystatechange = function () {//cuando el servidor este listo
     if (this.readyState == 4 && this.status == 200) {//si el servidor es el estado 4 y estatus 200 , si todo es ok
         //Transformamos
         var response = JSON.parse(xhttp.response); // Response es el JSON, luego lo parseo a objeto para que JS lo entienda, y lo meto en variable
+        console.log(response);
 
         for (i = 0; i < response.lenght; i++); {
             var posicion = 0;
-            hombreCalor = response.hombreCalor;
             var output = '';
-            output = `< img src = "${hombreCalor[i].name}" alt = "" > `
+            output = `${response[i].name}`
         }
+        console.log(output);
 
         ///Pintamos  el primer modelo
-        document.getElementById('mostradorMaletas').innerHTML = output;
+        document.getElementById('mostradorMaletas').src = output;
     }
 
-    ///-----------------btn que trae más modelos del mismo sexo y clima
 
+
+
+    ///-----------------btn que trae más modelos del mismo sexo y clima
     document.querySelector('.btn-roll').addEventListener('click', function otroModelo() {
         // numero ramdom
-        var dado = Math.floor(Math.random() * hombreCalor.length); //se pone así y no 7 , po que si no daca el indice 0, y en las img no lo tenemos asi
+        var dado = Math.floor(Math.random() * response.length); //se pone así y no 7 , po que si no daca el indice 0, y en las img no lo tenemos asi
 
         //Muestro el numero radom con la img, haciendo el truco de 0 y 1
         var contenedorCaja = document.querySelector('#mostradorMaletas');
-        document.getElementById('mostradorMaletas').src = `${hombreCalor[dado].name}`
-        //document.getElementById('mostradorMaletas').innerHTML = output = `< img src = "${hombreCalor[dado].name}" alt = "" > `
-        //mostradorMaletas.src = `< img src = "${dado}" alt = "" > `;
+        document.getElementById('mostradorMaletas').src = `${response[dado].name}`
+
     });
 
 };
@@ -89,14 +97,23 @@ xhttp.onreadystatechange = function () {//cuando el servidor este listo
 -Archivo Text, ph, xml, json, etc... 
 -true/ false: metodo de envio / es true = asincrono/*/
 
-xhttp.open("GET", "/json/hombre/calor/prendas.json", true);
+
+xhttp.open("GET", `/json/${genero}/${temperatura2}/prendas.json`, true);
 xhttp.send();
 
 
 
-///json/hombre/calor/img/1.jpg"
-let genero = "mujer";
-let temperatura2 = "frio";
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Funcion para traer cualquier JSON
